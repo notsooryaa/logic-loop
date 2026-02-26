69fb0e8831d83d2b1b1477dd9720212359147d75
notsooryaa <stunsooryaa@gmail.com>
2026-02-26 19:49:36 +0530

    Starter commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'logic-loop',
+  webDir: 'www'
+};
+
+export default config;
